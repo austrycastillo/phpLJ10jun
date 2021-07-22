098abf2b5d845fbb105f63eb639ade3f89afb5e8
Austry Castillo <castilloaustry@gmail.com>
2021-07-22 13:04:41 -0300

    Add files via upload
    clase 12

---
+++ clase12.php
@@ -0,0 +1,25 @@
+<?php
+//excepciones
+// function verificar($numero)
+// {
+//     if ($numero > 5 && $numero < 8)
+//         throw new Exception("Ups! el valor no puede ser 6 ni 7");
+//     elseif ($numero == 10)
+//         throw new Exception("Ups! el número no puede ser 10");
+//     return true;
+// }
+// try {
+//     $numero = 20;
+//     echo "iniciando el sistema...";
+//     verificar($numero);
+//     echo "<br>ejecutando blablabla...";
+//     echo "<br>codeando ;) <hr>";
+// } catch (Exception $e) {
+//     echo "<br>Error - " . $e->getMessage();
+// } finally {
+//     echo "<br>Finalizando sistema";
+// }
+
+
+//cookies
+setcookie()//aqui quedamos
